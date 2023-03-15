92816806204ea8e95b4f6f7509940c7ecc90068a
Paradox369 <george.ketcha@gmail.com>
2023-03-15 12:27:42 +0100

    reorganising code using controllers

---
+++ backend/controllers/workoutControllers.js
@@ -0,0 +1,76 @@
+const mongoose = require("mongoose");
+const workoutModel = require("../models/workoutModel");
+
+// get all workouts
+const getAllWorkouts = async (req, res) => {
+  const workouts = await workoutModel.find({}).sort({ createdAt: -1 });
+  res.status(200).json(workouts);
+};
+// get single workout
+const getSingleWorkout = async (req, res) => {
+  const { id } = req.params;
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+  const workout = await workoutModel.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+
+  res.status(200).json(workout);
+};
+// create new workout
+const createWorkout = async (req, res) => {
+  const { title, load, reps } = req.body;
+  try {
+    //adding work to db
+    const workout = await workoutModel.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+// delete a workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+
+  const workout = await workoutModel.findByIdAndDelete({ _id: id });
+  if (!workout) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+
+  res.status(200).json(workout);
+};
+// update a workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+  const workout = await workoutModel.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!workout) {
+    return res.status(404).json({ error: "no such workout" });
+  }
+
+  res.status(200).json(workout);
+};
+
+module.exports = {
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+  getSingleWorkout,
+  getAllWorkouts,
+};
--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,32 +1,22 @@
 const express = require("express");
-const workoutModel = require("../models/workoutModel");
+const {
+  getAllWorkouts,
+  getSingleWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutControllers");
 const router = express.Router();
 
 // get all workouts
-router.get("/", (req, res) => {
-  res.json({ msg: "get all workouts" });
-});
+router.get("/", getAllWorkouts);
 // get single workout
-router.get("/:id", (req, res) => {
-  res.json({ msg: "get single workout" });
-});
+router.get("/:id", getSingleWorkout);
 // post a workout
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-  try {
-    const workout = await workoutModel.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", createWorkout);
 // delete a workout
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "delete a workout" });
-});
+router.delete("/:id", deleteWorkout);
 // update a workout
-router.patch("/:id", (req, res) => {
-  res.json({ msg: "update a workout" });
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
